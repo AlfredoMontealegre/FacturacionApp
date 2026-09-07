a6a0a0240cc72076e61a83b12b5cecd8315df925
AlfredoMontealegre <montealfredo24@gmail.com>
2026-09-07 16:23:55 -0600

    add: Cargo

--- FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Cargo.java
+++ FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Cargo.java
@@ -1,4 +1,14 @@
 package ni.uam.edu.facturacionapp.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Cargo {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
 }
