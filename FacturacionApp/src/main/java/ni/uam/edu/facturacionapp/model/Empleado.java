21e5e7b48608bd2a7b37f14d80717b07cfb5d71a
AlfredoMontealegre <montealfredo24@gmail.com>
2026-09-07 16:24:02 -0600

    add: Empleado

--- FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Empleado.java
+++ FacturacionApp/src/main/java/ni/uam/edu/facturacionapp/model/Empleado.java
@@ -1,4 +1,18 @@
 package ni.uam.edu.facturacionapp.model;
 
+import java.time.LocalDate;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Empleado {
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private Cargo cargo;
+    private LocalDate fechaContratacion;
+    private boolean active;
 }
